5d549f5e324e35dee71d2b84cfe68d32e16dd191
klogdog <klogdog@users.noreply.github.com>
2016-02-16 10:30:39 -0500

    Create OTP.py

---
+++ OTP.py
@@ -0,0 +1,47 @@
+""" One time pad example. not cryptographically secure."""
+message = "this is an ultra secret message"
+import random
+a = []
+c = []
+d = []
+f = []
+g = []
+j= 0
+alphabetToNumber = {" " : 0, "a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10,"k":11,"l":12,"m":13,"n":14,"o":15,"p":16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26}
+numberToAlphabet = {0 : " ", 1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z'}
+#change the range to change the one time pad length
+for i in range(200):
+ randomthing = random.randint(1,26)
+ a.append(randomthing)
+
+print "one time pad"
+print a
+
+for i in message:
+  c.append(alphabetToNumber[i])
+  e = c[j] + a[j]
+  d.append(e)
+  j = j + 1
+
+print "message to number"
+print c
+print"encrypted message"
+print d
+
+j = 0
+
+for i in d:
+  e = d[j] - a[j]
+  f.append(e)
+  j = j + 1
+
+print "decrypted message"
+print f
+
+lengthF = len(f)
+for i in range(lengthF):
+    e = f[i]
+    g.append(numberToAlphabet[e])
+
+x = "".join(g)
+print x
