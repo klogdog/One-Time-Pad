817c08b8b7ba9c281267a55c25028601b4264517
klogdog <klogdog@users.noreply.github.com>
2017-05-09 07:07:10 -0400

    Update OTP.py

--- OTP.py
+++ OTP.py
@@ -15,7 +15,7 @@ alphabetToNumber = {" " : 0, "a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i"
 numberToAlphabet = {0 : " ", 1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z'}
 #change the range to change the one time pad length
 for i in range(200):
- randomthing = random.randint(1,26)
+ randomthing = random.randint(0,26)
  a.append(randomthing)
 
 print "one time pad"
